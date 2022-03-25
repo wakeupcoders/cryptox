1a5f853d8cbe5e833d8bfe4e0fbb73d78d756279
Sukhdeep Singh <ssingh7959@conestogac.on.ca>
2022-03-25 08:48:00 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ function CalcLumpsum() {
     let amount = document.getElementById('lAmount').value;
     let percentage = document.getElementById('lPercentage').value;
     let months = document.getElementById('lMonths').value;
+    console.log(amount)
+    console.log(percentage)
 
-    let result = (parseInt(amount) * parseInt(percentage) * parseInt(months)) / 100;
+    let result = parseInt(amount) * (parseInt(percentage) / 100);
 
-    result = parseInt(amount) * (1 + (parseInt(percentage) / 100) * parseInt(months));
+    //result = parseInt(amount) * (1 + (parseInt(percentage) / 100) * parseInt(months));
 
     document.getElementById('lResult').innerHTML = "Maturity Amount: $ " + result;
 }
